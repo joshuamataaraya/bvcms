d1b10c1da6530513e5969662d57b59da9bd04215
jfescobar18 <jfescobar18@hotmail.com>
2019-11-25 03:40:55 -0600

    NormalContributions label and location by Fund

---
+++ CmsData/Migrations/20191125_Alter_Func_NormalContributions.sql
@@ -0,0 +1,40 @@
+ALTER FUNCTION [dbo].[NormalContributions]
+(
+	@pid INT, 
+	@spid INT, 
+	@joint BIT, 
+	@fromDate DATE,
+	@toDate DATE,
+	@fundids VARCHAR(MAX),
+	@TaxStatusCategorization BIT
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT 
+		c.ContributionId,
+		c.ContributionAmount,
+		c.ContributionDate,
+		f.FundName as FundName,
+		c.CheckNo,
+		p.[Name],
+		[Description] = CASE WHEN @TaxStatusCategorization = 1 THEN f.FundDescription ELSE c.ContributionDesc END,
+		CASE WHEN @TaxStatusCategorization = 1 THEN f.FundDescription + ' (Tax Deductible)' ELSE f.FundDescription END AS FundDescription
+	FROM dbo.Contribution c
+	JOIN dbo.ContributionFund f ON f.FundId = c.FundId
+	JOIN dbo.People p ON p.PeopleId = c.PeopleId
+	WHERE 1=1
+	AND c.ContributionStatusId = 0 -- Recorded
+	AND c.ContributionTypeId NOT IN (6, 7) -- not returned or reversed
+	AND c.contributionTypeId NOT IN (10, 20) -- not stock or giftinkind
+	AND c.ContributionTypeId NOT IN (8, 9) -- not pledge or nontaxded
+	AND ISNULL(f.NonTaxDeductible, 0) = 0
+	AND c.ContributionDate >= @fromDate
+	AND CONVERT(DATE, c.ContributionDate) <= @toDate
+	AND (c.PeopleId = @pid OR (@joint = 1 AND c.PeopleId = @spid))
+	AND (@fundids IS NULL OR EXISTS(SELECT NULL FROM dbo.SplitInts(@fundids) WHERE Value = c.FundId))
+)
+GO
+
+
